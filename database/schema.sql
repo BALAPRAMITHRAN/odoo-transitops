e453e9c71096d7f5ed8345be3a49e3f309fc0991
erlinar04 <erlinar410@gmail.com>
2026-07-12 11:57:12 +0530

    added db schema and seed files

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(...);
+
+CREATE TABLE roles(...);
+
+CREATE TABLE vehicles(...);
+
+CREATE TABLE drivers(...);
+
+CREATE TABLE trips(...);
+
+CREATE TABLE maintenance_logs(...);
+
+CREATE TABLE fuel_logs(...);
+
+CREATE TABLE expenses(...);
---
+++ database/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO roles(...);
+
+INSERT INTO users(...);
+
+INSERT INTO vehicles(...);
+
+INSERT INTO drivers(...);
+
+INSERT INTO trips(...);
